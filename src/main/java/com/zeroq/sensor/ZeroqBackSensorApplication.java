771a810fc35be654bd3bbcef34cd24cb7a7e37f4
김도영 <zz01064842675@gmail.com>
2026-01-26 13:43:21 +0900

    feat: init

---
+++ src/main/java/com/zeroq/sensor/ZeroqBackSensorApplication.java
@@ -0,0 +1,13 @@
+package com.zeroq.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroqBackSensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroqBackSensorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeroq/sensor/ZeroqBackSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroq.sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroqBackSensorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
